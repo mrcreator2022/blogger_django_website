c852747b50d21406f54162f15e1642e862dcda98
mrcreator2022 <88509618+mrcreator2022@users.noreply.github.com>
2022-09-30 03:10:00 +0530

    initial commit

---
+++ blogmanagement/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    image = models.ImageField(upload_to='images/')
+
+
+
+
+
